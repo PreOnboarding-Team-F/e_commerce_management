b09d40f312bf10f37a0e495364dcacdda58eb54f
hyo-jin-jeong <55984573+hyo-jin-jeong@users.noreply.github.com>
2022-11-01 02:00:58 +0900

    Test: delivery controller unit test 구현

---
+++ __test__/unit/delivery.controller.test.js
@@ -0,0 +1,28 @@
+import * as deliveryController from '../../controller/delivery.js';
+import * as deliveryService from '../../service/delivery.js';
+
+import httpMocks from 'node-mocks-http';
+
+jest.mock('../../service/delivery.js');
+describe('order controller delivery status update', () => {
+  it('성공했을 때', async () => {
+    const orderId = 1;
+    const deliveryStatus = 1;
+    const request = httpMocks.createRequest({
+      url: '/order/delivery-status',
+      method: 'PATCH',
+      body: {
+        orderId,
+        deliveryStatus,
+      },
+    });
+    const response = httpMocks.createResponse();
+    deliveryService.updateDeliveryStatus = jest.fn();
+
+    await deliveryController.updateDeliveryStatus(request, response);
+
+    expect(deliveryService.updateDeliveryStatus).toBeCalledTimes(1);
+    expect(response.statusCode).toBe(200);
+    expect(response._getJSONData().message).toBe('UPDATE SUCCESS');
+  });
+});
