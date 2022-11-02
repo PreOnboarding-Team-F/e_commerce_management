8292deae78cd2cb57976e233fe03bf6a3dbf585a
박주현 <leehot2665@naver.com>
2022-11-02 09:06:34 +0900

    Fix : 주문날짜 확인하는 조건 bug 수정

--- controller/order.js
+++ controller/order.js
@@ -6,8 +6,16 @@ async function getOrders(req, res) {
   const startDate = req.query?.startdate;
   const endDate = req.query?.enddate;
   const orderStatus = req.query.orderstatus;
-  if (!startDate ^ !endDate || startDate.length !== 8 || endDate.length !== 8) {
+  if (!startDate ^ !endDate) {
     throw new BadRequestException('주문날짜 시작과 끝을 모두 보내주세요');
+  } else {
+    if (
+      startDate &&
+      endDate &&
+      (startDate.length !== 8 || endDate.length !== 8)
+    ) {
+      throw new BadRequestException('주문날짜 시작과 끝을 모두 보내주세요');
+    }
   }
   const data = await ordersService.getOrders({
     name,
