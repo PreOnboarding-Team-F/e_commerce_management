ec0784b72d4eef22bf2f19c7885be32595edf90f
박주현 <leehot2665@naver.com>
2022-11-02 19:34:38 +0900

    Refacor : 라우터에서 상태 변경 req 유효성 검사 추가

--- route/order.js
+++ route/order.js
@@ -1,9 +1,30 @@
 import express from 'express';
 import ordersController from '../controller/order.js';
+import { ORDER_STATUS } from '../model/orderStatus.js';
+import { body } from 'express-validator';
+import validator from '../middleware/validate.js';
 
 const router = express.Router();
 
+const orderStatusUpdate = [
+  body('orderStatus')
+    .notEmpty()
+    .withMessage('유효하지 않은 값입니다.')
+    .trim()
+    .custom(value => {
+      if (
+        value === ORDER_STATUS.CANCEL ||
+        value === ORDER_STATUS.COMPLITE ||
+        value === ORDER_STATUS.SEND
+      ) {
+        return true;
+      }
+    })
+    .withMessage('유효하지 않은 값입니다.'),
+  validator,
+];
+
 router.get('', ordersController.getOrders);
-router.patch('/:id', ordersController.updateOrderStatus);
+router.patch('/:id', orderStatusUpdate, ordersController.updateOrderStatus);
 
 export default router;
